709823d18f98aa29693695b436797c8888e394f0
Pedro Rufino Segovia <pedro.rufino.segovia@gmail.com>
2019-03-13 15:42:44 +0100

    cambio de tamaño

--- Monedas.py
+++ Monedas.py
@@ -1,12 +1,12 @@
 from tkinter import *
-
+from tkinter import ttk
 class Imputs(ttk.Frame):
     def __init__(self, parent, **args):
         ttk.Frame__init__
 class Mainapp(Tk):
     def __init__(self):
         Tk.__init__(self)
-        self.geometry("300x400")
+        self.geometry("378x229")
         self.title("Cambio de Monedas")
     def start(self):
         self.mainloop()
